320caa0f2b0dee17eb0809d625ddff3b51dd3244
ShiderS <irbis26813@gmail.com>
2024-04-20 20:10:14 +0500

    Added message distribution by the administrator 1.1 (fix)
    edited .py files:
    -main

--- main.py
+++ main.py
@@ -99,25 +99,26 @@ async def support(message: types.Message):
 
 @dp.message()
 async def handle_message(message: types.Message):
-    developer_id = sorted(
-        {i.id: i.workload for i in DB_SESS.query(User).filter(User.is_developer).all()}.items(),
-        key=lambda x: x[1])[0][0]
-    admin = DB_SESS.query(User).filter(User.id == developer_id).first()
     # Проверяем, обратился ли пользователь в поддержку
     global in_answer
     if in_answer[0]:
-        await bot.send_message(in_answer[1], message.text)
-        await message.reply("Ответ отправлен пользователю")
+        admin = DB_SESS.query(User).filter(User.id == message.from_user.id).first()
         admin.workload -= 1
         DB_SESS.commit()
+        await bot.send_message(in_answer[1], message.text)
+        await message.reply("Ответ отправлен пользователю")
         users_in_support.remove(in_answer[1])
         in_time.remove(in_answer[1])
         in_answer = [False, 0]
     elif message.from_user.id in users_in_support and message.from_user.id not in in_time:
         ##################
-        admin_message = f"Пользователь {message.from_user.first_name} задал вопрос: {message.text}"
+        developer_id = sorted(
+            {i.id: i.workload for i in DB_SESS.query(User).filter(User.is_developer).all()}.items(),
+            key=lambda x: x[1])[0][0]
+        admin = DB_SESS.query(User).filter(User.id == developer_id).first()
         admin.workload += 1
         DB_SESS.commit()
+        admin_message = f"Пользователь {message.from_user.first_name} задал вопрос: {message.text}"
         await bot.send_message(developer_id, admin_message, reply_markup=markup_for_admin_ans(message.from_user.id).as_markup())
         in_time.append(message.from_user.id)
         ##################
@@ -134,7 +135,11 @@ async def callbacks_num_change_fab(callback: types.CallbackQuery, callback_data:
     if callback_data.action == "ok":
         await callback.message.edit_text(f"Напишите текст для ответа:")
         in_answer = [True, callback_data.id]
+
     elif callback_data.action == "cancel":
+        admin = DB_SESS.query(User).filter(User.id == callback.message.from_user.id).first()
+        admin.workload -= 1
+        DB_SESS.commit()
         await bot.send_message(callback_data.id, "Вопрос отклонён")
         await callback.message.edit_text(f"Вопрос отклонён")
         users_in_support.remove(callback_data.id)
